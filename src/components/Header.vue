cc296b59ceb267826c661ef65fa83b728e044d31
webster <843835335@qq.com>
2018-11-07 16:04:49 +0800

    appHeader

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,49 @@
 <template>
-    <div class="header">
-        this is header
+  <div class="header">
+    <div class="header-left">
+        <span class="iconfont">&#xe624;</span>
     </div>
+    <div class="header-input">
+        <span class="iconfont">&#xe632;</span>输入城市景点/游玩主题
+    </div>
+    <div class="header-right">城市<span class="iconfont">&#xe64a;</span></div>
+  </div>
 </template>
 
 <script>
+import '../assets/styles/varibles.css'
 export default {
-    name : 'HomeHeader'
+  name : 'HomeHeader'
 }
 </script>
 
-<style lang="stylus" scoped>
-    
+<style scoped>
+  .header{
+      width: 100%;
+      display: flex;
+      background: var(--bgColor);
+      color: #fff;
+      line-height: 64px;
+  }
+  .header-left{
+      width : 48px;
+      float: left;
+      text-align: center;
+  }
+  .header-input{
+      flex: 1;
+      height: 40px;
+      background: #fff;
+      border-radius: 8px;
+      margin-top: 12px;
+      margin-left: 4px;
+      padding-left: 4px;
+      color: #ccc;
+      line-height: 40px;
+  }
+  .header-right{
+      width: 64px;
+      float: right;
+      text-align: center;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body);
